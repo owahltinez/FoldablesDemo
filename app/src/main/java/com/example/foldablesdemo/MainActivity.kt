f19c93f7a0d654e7df6a6c97f4117648de60e385
Oscar Wahltinez <oscar@wahltinez.org>
2021-10-18 19:32:28 -0400

    Add multi-instance activity launch example

--- app/src/main/java/com/example/foldablesdemo/MainActivity.kt
+++ app/src/main/java/com/example/foldablesdemo/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.foldablesdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button_launch)
+        button.setOnClickListener {
+            startActivity(Intent(this, SecondaryActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT
+            })
+        }
     }
 }
